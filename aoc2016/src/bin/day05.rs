f96d46c5c7d5dde129743afc4b28dfacfed3f836
Deep Kr. Ghosh <75083264+deepkrg17@users.noreply.github.com>
2024-01-25 09:35:50 +0530

    AoC2016D05

---
+++ aoc2016/src/bin/day05.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let id = "ffykfhsq";
+    let mut p1 = Vec::with_capacity(8);
+    let mut p2 = Vec::with_capacity(8);
+    p2.resize(8, String::new());
+    for h in (0..)
+        .map(|i| md5::compute(id.to_owned() + &i.to_string()))
+        .filter(|h| h[0] == 0 && h[1] == 0 && (h[2] & 0xF0) == 0)
+    {
+        let f = h[2] & 0x0F;
+        if p1.len() < 8 {
+            p1.push(format!("{:x?}", f));
+        }
+        if f < 8 && p2[f as usize].is_empty() {
+            p2[f as usize] = format!("{:x?}", h[3] >> 4);
+            if p2.iter().all(|d| !d.is_empty()) {
+                break;
+            }
+        }
+    }
+    println!("part 1: {}", p1.into_iter().collect::<String>());
+    println!("part 2: {}", p2.into_iter().collect::<String>());
+}
